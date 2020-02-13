11e1a31736a6b70df7f3b113702ff503c1371e28
Euan Deas <euandeas@hotmail.com>
2020-02-13 13:37:34 +0000

    Add project files.

---
+++ DefaultMod/ModMain.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// 1. Added the "GooseModdingAPI" project as a reference.
+// 2. Compile this.
+// 3. Create a folder with this DLL in the root, and *no GooseModdingAPI DLL*
+using GooseShared;
+using SamEngine;
+
+namespace BreadCrumbs
+{
+    public class ModEntryPoint : IMod
+    {
+        // Gets called automatically, passes in a class that contains pointers to
+        // useful functions we need to interface with the goose.
+        void IMod.Init()
+        {
+            // Subscribe to whatever events we want
+            InjectionPoints.PostTickEvent += PostTick;
+        }
+
+        public void PostTick(GooseEntity g)
+        {
+            // Do whatever you want here.
+
+        }
+    }
+}

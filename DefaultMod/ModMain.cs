e2efc7af5089f2bd7494a46c8a89fe1baebf066c
Euan Deas <euandeas@hotmail.com>
2020-02-13 14:29:45 +0000

    Started adding in picture box

--- DefaultMod/ModMain.cs
+++ DefaultMod/ModMain.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 // 1. Added the "GooseModdingAPI" project as a reference.
 // 2. Compile this.
 // 3. Create a folder with this DLL in the root, and *no GooseModdingAPI DLL*
@@ -13,6 +15,10 @@ namespace BreadCrumbs
 {
     public class ModEntryPoint : IMod
     {
+        [DllImport("user32.dll")]
+        public static extern short GetAsyncKeyState(Keys vKey);
+
+        bool feedOut = false;
         // Gets called automatically, passes in a class that contains pointers to
         // useful functions we need to interface with the goose.
         void IMod.Init()
@@ -24,7 +30,19 @@ namespace BreadCrumbs
         public void PostTick(GooseEntity g)
         {
             // Do whatever you want here.
-
+            if (GetAsyncKeyState(Keys.T) != 0)
+            {
+                if (!feedOut)
+                {
+                    var f = new Form();
+                    f.FormBorderStyle = FormBorderStyle.None;
+                    f.Size = new System.Drawing.Size(85, 85);
+                    f.Location = new System.Drawing.Point(200, 200);
+                    f.Controls.Add(new PictureBox() { ImageLocation = @"crumbs.jpg", SizeMode = PictureBoxSizeMode.AutoSize });
+                    f.Show();
+                    feedOut = true;
+                }
+            }
         }
     }
 }

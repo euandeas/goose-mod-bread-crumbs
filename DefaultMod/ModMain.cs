bf6fccf86be78a8f518590b6df09704c01700db0
Euan Deas <euandeas@hotmail.com>
2020-02-14 15:03:55 +0000

    Basixc Functionality Finished

--- DefaultMod/ModMain.cs
+++ DefaultMod/ModMain.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -20,6 +21,10 @@ namespace BreadCrumbs
         public static extern short GetAsyncKeyState(Keys vKey);
 
         bool feedOut = false;
+        int targetX;
+        int targetY;
+
+        Form f;
         // Gets called automatically, passes in a class that contains pointers to
         // useful functions we need to interface with the goose.
         void IMod.Init()
@@ -35,18 +40,65 @@ namespace BreadCrumbs
             {
                 if (!feedOut)
                 {
-                    
-                    var f = new Form
-                    {
-                        FormBorderStyle = FormBorderStyle.None,
-                        Size = new Size(85, 85),
-                        Location = new Point(Control.MousePosition.X, Control.MousePosition.Y),
-                        MinimumSize = new Size(80,80)
-                    };
+                    int mouseX = Cursor.Position.X;
+                    int mouseY = Cursor.Position.Y;
+                    targetX = Input.mouseX;
+                    targetY = Input.mouseY;
+
+                    CreateForm();
+                    f.BringToFront();
+                    f.DesktopLocation = new Point(mouseX - 42, mouseY - 42);
                     f.Controls.Add(new PictureBox() { ImageLocation = @"crumbs.jpg", SizeMode = PictureBoxSizeMode.AutoSize });
                     f.Show();
+                    g.targetPos = new Vector2(targetX,targetY);
                     feedOut = true;
                 }
+            }
+            
+            if (feedOut)
+            {
+                g.targetPos = new Vector2(targetX, targetY);
+
+                if (IsGooseOnFood(g))
+                {
+                    f.Dispose();
+                    feedOut = false;
+                }
+            }
+
+        }
+
+        private void CreateForm()
+        {
+            f = new Form
+            {
+                FormBorderStyle = FormBorderStyle.None,
+                Size = new Size(85, 85),
+                StartPosition = FormStartPosition.Manual,
+                MinimumSize = new Size(80, 80)
+            };
+        }
+
+        public bool IsGooseOnFood(GooseEntity g)
+        {
+            float xDif = g.position.x - g.targetPos.x;
+            float yDif = g.position.y - g.targetPos.y;
+
+            using (StreamWriter sr = new StreamWriter("CDriveDirs.txt"))
+            {
+                sr.WriteLine(xDif);
+                sr.WriteLine(yDif);
+                sr.WriteLine("");
+            }
+
+
+            if (((-50 < xDif) && (xDif < 50)) && ((-50 < yDif) && (yDif < 50)))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
             }
         }
     }

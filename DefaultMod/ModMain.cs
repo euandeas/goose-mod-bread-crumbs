dc7ef7700bdcd2fb0ec9727d06d7f8fbbb69322f
GamingInfinite <52724070+GamingInfinite@users.noreply.github.com>
2020-02-28 06:47:37 -0600

    Update ModMain.cs
    This is my code for the config plus your mod.  One more coming down the pipe.

--- DefaultMod/ModMain.cs
+++ DefaultMod/ModMain.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using GooseShared;
+using SamEngine;
 using System.Drawing;
 using System.IO;
 using System.Media;
@@ -6,124 +7,115 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Threading;
 using System.Windows.Forms;
-using GooseShared;
-using SamEngine;
+using System;
 
-namespace BreadCrumbs
+public class ModMain : IMod
 {
-    public class ModEntryPoint : IMod
-    {
-        [DllImport("user32.dll")]
-        public static extern short GetAsyncKeyState(Keys vKey);
+	private bool feedOut;
 
-        private bool feedOut = false;
-        private Vector2 targetVector;
-        private Point pointOfCrumbs;
+	private Vector2 targetVector;
 
-        private string SoundFileName;
-        private Image theImage;
+	private Point pointOfCrumbs;
 
-        private int tickCount;
-        private string keyBind;
-        private int imageSize;
-        void IMod.Init()
-        {
-            string assemblyFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            string PicFileName = Path.Combine(assemblyFolder, "crumbs.png");
-            SoundFileName = Path.Combine(assemblyFolder, "nom.wav");
-            theImage = Image.FromFile(PicFileName);
+	private string SoundFileName;
 
-            CheckConfig(assemblyFolder);
+	private Image theImage;
 
-            InjectionPoints.PostTickEvent += PostTick;
-            InjectionPoints.PreRenderEvent += PreRenderEvent;
-        }
+	private int tickCount;
 
-        private void CheckConfig(string assemFolder)
-        {
-            string path = Path.Combine(assemFolder, "Config.txt");
-            try
-            {
-                using (TextReader textReader = new StreamReader(new FileStream(path, FileMode.Open)))
-                {
-                    string text;
-                    while ((text = textReader.ReadLine()) != null)
-                    {
-                        if (text.StartsWith("KeyName"))
-                        {
-                            int num = text.IndexOf("=") + 1;
-                            keyBind = text.Substring(num, text.Length - num).Trim();
-                        }
-                        if (text.StartsWith("ImageSize"))
-                        {
-                            int num = text.IndexOf("=") + 1;
-                            imageSize = Convert.ToInt32(text.Substring(num, text.Length - num).Trim());
-                        }
-                    }
-                }
-            }
-            catch
-            {
-                using (StreamWriter streamWriter = File.Exists(path) ? File.AppendText(path) : File.CreateText(path))
-                {
-                    MessageBox.Show("Config.txt for BreadCrumbs was not found.\nMaking config file please restart", "Mod Error", MessageBoxButtons.OK, MessageBoxIcon.Hand);
-                    streamWriter.WriteLine("KeyName=RShiftKey");
-                    streamWriter.WriteLine("ImageSize=80");
-                }
-            }
-        }
+	private Config config = new Config();
 
-        private void PreRenderEvent(GooseEntity goose, Graphics g)
-        {
-            if (feedOut)
-            {
-                g.DrawImage(theImage, pointOfCrumbs.X, pointOfCrumbs.Y, imageSize, imageSize);
-            } 
-        }
+	KeysConverter kc = new KeysConverter();
 
-        public void PostTick(GooseEntity g)
-        {
-            if ((GetAsyncKeyState((Keys)Enum.Parse(typeof(Keys), keyBind, true)) != 0) && !feedOut)
-            {
-                feedOut = true;
-                targetVector = new Vector2(Input.mouseX- (imageSize / 4), Input.mouseY+(imageSize / 4));
-                pointOfCrumbs = new Point(Cursor.Position.X - (imageSize / 2), Cursor.Position.Y - (imageSize / 2));
+	[DllImport("user32.dll")]
+	public static extern short GetAsyncKeyState(Keys vKey);
 
-                API.Goose.playHonckSound();
-                g.targetPos = targetVector;
-                API.Goose.setTaskRoaming(g);
-                API.Goose.setSpeed(g, GooseEntity.SpeedTiers.Charge);
-            }
-            if (feedOut)
-            {
-                if (API.Goose.isGooseAtTarget(g,10))
-                {
-                    tickCount++;
-                    g.direction = -20;
-                    API.Goose.setSpeed(g, GooseEntity.SpeedTiers.Walk);
-                    API.Goose.setTaskRoaming(g);
+	private void checkConfig() {
+		string path = "Assets\\Mods\\BreadCrumbs\\Config.txt";
 
-                    if (tickCount == 240)
-                    {
-                        new Thread(() => {
-                            using (SoundPlayer player = new SoundPlayer(SoundFileName))
-                            {
-                                player.PlaySync();
-                            }
-                        }).Start(); 
+		try {
+			using (TextReader textReader = new StreamReader(new FileStream(path, FileMode.Open))) {
+				string text;
+				while ((text = textReader.ReadLine()) != null) {
+					if (text.StartsWith("KeyName")) {
+						int num = text.IndexOf("=") + 1;
+						string keybind = text.Substring(num, text.Length - num).Trim();
+						config.KeyName = keybind; 
+					}
+				}
+			}
+		}
+		catch
+		{
+			using (StreamWriter streamWriter = File.Exists(path) ? File.AppendText(path) : File.CreateText(path))
+			{
+				MessageBox.Show("Config.txt for BreadCrumbs was not found.\nMaking config file please restart", "Mod Error", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+				streamWriter.WriteLine("KeyName=RShiftKey");
+			}
+		}
+	}
 
-                        feedOut = false;
-                        tickCount = 0;
-                        g.targetPos = targetVector + new Vector2(20, -20);
-                    }   
-                }
-                else
-                {
-                    API.Goose.setTaskRoaming(g);
-                    API.Goose.setSpeed(g, GooseEntity.SpeedTiers.Charge);
-                    g.targetPos = targetVector;
-                }
-            }
-        }
-    }
+	void IMod.Init()
+	{
+		string directoryName = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+		string filename = Path.Combine(directoryName, "crumbs.png");
+		SoundFileName = Path.Combine(directoryName, "nom.wav");
+		theImage = Image.FromFile(filename);
+		checkConfig();
+		InjectionPoints.PostTickEvent += new InjectionPoints.PostTickEventHandler(this.PostTick);
+		InjectionPoints.PreRenderEvent += new InjectionPoints.PreRenderEventHandler(this.PreRenderEvent);
+	}
+
+	private void PreRenderEvent(GooseEntity goose, Graphics g)
+	{
+		if (feedOut)
+		{
+			g.DrawImage(theImage, pointOfCrumbs.X, pointOfCrumbs.Y, 80, 80);
+		}
+	}
+
+	public void PostTick(GooseEntity g)
+	{
+		
+		if (GetAsyncKeyState((Keys)Enum.Parse(typeof(Keys), config.KeyName, true)) != 0 && !feedOut)
+		{
+			feedOut = true;
+			targetVector = new Vector2((float)(Input.mouseX - 20), (float)(Input.mouseY + 20));
+			pointOfCrumbs = new Point(Cursor.Position.X - 42, Cursor.Position.Y - 42);
+			API.Goose.playHonckSound.Invoke();
+			g.targetPos = targetVector;
+			API.Goose.setTaskRoaming.Invoke(g);
+			API.Goose.setSpeed.Invoke(g, (GooseEntity.SpeedTiers)2);
+		}
+		if (!feedOut)
+		{
+			return;
+		}
+		if (API.Goose.isGooseAtTarget.Invoke(g, 10f))
+		{
+			tickCount++;
+			g.direction = -20f;
+			API.Goose.setSpeed.Invoke(g, (GooseEntity.SpeedTiers)0);
+			API.Goose.setTaskRoaming.Invoke(g);
+			if (tickCount == 240)
+			{
+				new Thread((ThreadStart)delegate
+				{
+					using (SoundPlayer soundPlayer = new SoundPlayer(SoundFileName))
+					{
+						soundPlayer.PlaySync();
+					}
+				}).Start();
+				feedOut = false;
+				tickCount = 0;
+				g.targetPos = targetVector + new Vector2(20f, -20f);
+			}
+		}
+		else
+		{
+			API.Goose.setTaskRoaming.Invoke(g);
+			API.Goose.setSpeed.Invoke(g, (GooseEntity.SpeedTiers)2);
+			g.targetPos = targetVector;
+		}
+	}
 }
